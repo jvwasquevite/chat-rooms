fe698834bf19dab8f901403a39cefa8fbe5e69fe
João <jvwasquevite@gmail.com>
2021-07-26 11:51:28 -0300

    feat: imported socket.io and created first view

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
 import express from 'express'
+import path from 'path'
+
+import { createServer } from 'http'
+import { Socket, Server } from 'socket.io'
 
 const app = express()
 app.use(express.json())
+
+const server = createServer(app)
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.set('views', path.join(__dirname, '..', 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+  res.render('index.html')
+})
 
 app.listen(3000, () => console.log('Server is running'))
